24bb43a9af92d2b6a4eebf9847d2629b573d77b4
FenixTfive <fenix._w05@hotmail.com>
2022-10-11 14:08:58 -0500

    entrenador y generador de modelos IA

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 #--------------------------------------------------------------------------
 import json
 from statistics import mode
+import os
 #extra html
 import urllib.request
 #get only text data from html
@@ -16,6 +17,9 @@ import tensorflow as tf
 from keras.preprocessing.text import Tokenizer
 from keras_preprocessing.sequence import pad_sequences
 import numpy as np
+#custom local function
+import gen_model
+from gen_model import model_generator
 
 #--------------------------------------------------------------------------
 #---------------------- variables -----------------------------------------
@@ -29,6 +33,8 @@ trunc_type = 'post'
 padding_type = 'post'
 oov_tok = "<OOV>"
 training_size = 20000  # we take a short % from the existing lines in sarcasm.json
+#get current path
+currentPath = os.getcwd()
 
 #*****************************************************************************************************
 #                             OPEN , AND GET TEXT FROM URL
@@ -94,7 +100,9 @@ result = result.lower()
 #**************load training json data  - example https://storage.googleapis.com/laurencemoroney-blog.appspot.com/sarcasm.json from https://rishabhmisra.github.io/publications/
 #--------------------------------------------------------------------------
 
-with open("./stereotype.json", 'r', encoding='cp437') as f:
+with open(currentPath + "/text_clasification/stereotype.json",
+          'r',
+          encoding='cp437') as f:
     datastore = json.load(f)
 
 for item in datastore:
@@ -140,63 +148,28 @@ training_labels = np.array(training_labels)
 testing_padded = np.array(testing_padded)
 testing_labels = np.array(testing_labels)
 
-#-----------------------------------------------------------------------------
-#-------- AQUI HACEMOS EMBEDING -    NEURAL NETWORK CODE ---------------------
-#-----------------------------------------------------------------------------
-#
-#Un concepto que busca explicar como la informacion segun lo establecido puede ir de un punto a otro como un vector
-# es decir supongamos que testeamos algo que es bueno o malo
-#hay 3 caminos :
-#   * izquierda (malo)
-#   * centro (neutral)
-#   * derecha (bueno)
-#pero si lo representamos como un vector https://www.google.com/search?q=vectores&rlz=1C1CHZN_esMX957MX957&sxsrf=ALiCzsZjUc9_82_CvaW_Gm9ecpIQ5-lZ2g:1664841867297&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiF0-2go8X6AhVcMEQIHRw6BIsQ_AUoAXoECAEQAw&biw=1360&bih=635&dpr=1
-# su posicion cambiaria segun lo establecido y podriamos obtener un texto
-# bueno [1,0]
-#  malo [-1,0],
-# ligeramente bueno inclinqeo q neutras [0.7,0.7]
-#                  Y
-#                  |
-#                  |
-#                  | *[0.7,0.7]
-#                  |
-#                  |
-#                  |
-#                  |
-# [-1,0]-------------------------- X [1,0]
+#***********************************************************************************************************************************************************
+#                               AQUI HACEMOS EMBEDING -    NEURAL NETWORK CODE                       VER OPCIONES
+#***********************************************************************************************************************************************************
+#OPCION 1
+#SI YA TENEMOS UN MODELO DESCARGADO Y ENTREDADO , PODEMOS CARGAR EN LA VARIABLE MODEL
+model = tf.keras.models.load_model(currentPath +
+                                   "/text_clasification/model_trained")
+#OPCION 2
+#SI NO TE SE TIENE UN MODELO , PODEMOS CONSTRUIRLO Y ENTRENARLO DE 0 , LLAMANDO LA SIGUIENTE FUNCIO Y LUEGO , CARGAR ESE MODELO EN LA VARIABLE MODEL
+# model_generator(vocab_size=vocab_size,
+#                 embedding_dim=embedding_dim,
+#                 max_length=max_length,
+#                 training_padded=training_padded,
+#                 training_labels=training_labels,
+#                 testing_padded=testing_padded,
+#                 testing_labels=testing_labels)
 
-#NEURAL NETWORK CODE
-model = tf.keras.Sequential([
-    tf.keras.layers.Embedding(vocab_size,
-                              embedding_dim,
-                              input_length=max_length),
-    tf.keras.layers.GlobalAveragePooling1D(),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(32, activation='relu'),
-    tf.keras.layers.Dense(1, activation='relu'),
-])
-model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
-
-model.summary()
-model.save("./model_trained")
-
-#------------------------------------------------------------------------------------------
-#-----AQUI ENTRENAMOS A LA IA - mediante ephochs (ciclos de entrenamiento para una IA)-----
-#------------------------------------------------------------------------------------------
-#
-#  https://ciberseguridad.com/guias/nuevas-tecnologias/machine-learning/epoch/
-
-num_epochs = 30
-history = model.fit(training_padded,
-                    training_labels,
-                    epochs=num_epochs,
-                    validation_data=(testing_padded, testing_labels),
-                    verbose=2)
+# model = tf.keras.models.load_model(currentPath +
+#                                    "/text_clasification/model_trained")
+#***********************************************************************************************************************************************************
+#                             FIN   AQUI HACEMOS EMBEDING -    NEURAL NETWORK CODE
+#***********************************************************************************************************************************************************
 
 #--------------------------------------------------------------------------
 #-- AQUI PODEMOS IMPRIMIR GRAFICAS DEL COMPORTAMIENTO DE LOS CICLOS--------
